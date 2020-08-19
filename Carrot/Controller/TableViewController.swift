155ce11871e5051d4c87a19725ce374474104aee
mattttis <63421937+mattttis@users.noreply.github.com>
2020-08-19 14:55:03 +0200

    Removing item works

--- Carrot/Controller/TableViewController.swift
+++ Carrot/Controller/TableViewController.swift
@@ -203,15 +203,7 @@ class TableViewController: UITableViewController, AddTask, ChangeButton, UITable
         let delete = deleteAction(at: indexPath)
         return UISwipeActionsConfiguration(actions: [delete])
     }
-    
-//    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-//        return .none
-//    }
-//
-//    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
-//        return false
-//    }
-    
+
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         
         let movedObject = self.sections[sourceIndexPath.section].items[sourceIndexPath.row]
@@ -411,6 +403,73 @@ class TableViewController: UITableViewController, AddTask, ChangeButton, UITable
                     } else {
                         print("Document successfully written!")
                         
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                            print("Moving item to itemsChecked...")
+                            
+                            let item = self.sections[indexSection!].items[indexRow!]
+
+                                print("Moving item to itemsChecked2...")
+                                // Item's properties variables
+                                let itemID: String
+                                let name: String
+                                let uid: String
+                                let isChecked: Bool
+                                let checkedBy: String
+                                let dateCreated: Date
+                                let dateChecked: Date
+                                
+                                let itemRef = self.db.collection(K.FStore.lists).document(self.currentListID!).collection(K.FStore.sections).document("\(indexSection!)").collection(K.FStore.items).document(item.itemID!)
+                                
+                                itemRef.getDocument { (document, error) in
+                                    if let document = document, document.exists {
+                                        let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                                        
+                                        print("Moving item to itemsChecked3...")
+                                        
+                                        // Get the properties of the item
+                                        let name = document.data()?[K.Item.name] as? String
+                                        let uid = document.data()?[K.Item.uid] as? String
+                                        let category = document.data()?[K.Item.categoryNumber] as? Int
+                                        let isChecked = document.data()?[K.Item.isChecked] as? Bool
+                                        let dateCreated = document.data()?[K.Item.date] as? Date
+                                        let dateChecked = document.data()?[K.Item.dateChecked] as? Date
+                                        let checkedBy = document.data()?[K.Item.checkedBy] as? String
+                                        
+                                        var ref: DocumentReference? = nil
+                                        
+                                        // Save the properties of the item in sectionsDeleted
+                                        ref = self.db.collection(K.lists).document(self.currentListID!).collection(K.FStore.sectionsChecked).document("\(category!)").collection(K.FStore.items).addDocument(data: [
+                                                K.Item.name: name,
+                                                K.Item.isChecked: isChecked,
+                                                K.Item.categoryNumber: category,
+                                                K.Item.date: dateCreated,
+                                                K.Item.dateChecked: dateChecked,
+                                                K.Item.checkedBy: checkedBy,
+                                                K.Item.uid: uid,
+                                                K.Item.dateDeleted: Date()
+                                        ]) { err in
+                                            if let err = err {
+                                                print("Error adding document: \(err)")
+                                            } else {
+
+                                                // If successfull, delete the item in the normal collection
+                                                itemRef.delete() { err in
+                                                    if let err = err {
+                                                        print("Error removing document: \(err)")
+                                                    } else {
+                                                        print("Document successfully removed!")
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    } else {
+                                        print("Document does not exist")
+                                    }
+                                }
+                                
+                                self.tableView.reloadData()
+                        }
+                        
                     }
                 }
             } else {
@@ -421,8 +480,6 @@ class TableViewController: UITableViewController, AddTask, ChangeButton, UITable
                         print("Error writing document: \(err)")
                     } else {
                         print("Document successfully written!")
-//                        let generator = UINotificationFeedbackGenerator()
-//                        generator.notificationOccurred(.success)
                     }
                 }
             }
